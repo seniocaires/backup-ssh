5f169c15aa334aeb8d27fc237d48272ee83fd12d
Senio Caires <seniocaires@gmail.com>
2017-11-10 14:07:33 -0200

    EoL windows - linux

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 sshpass -p $PASSWORD rsync -avz -e "/usr/bin/ssh -p $PORT -t -vvvv -oStrictHostKeyChecking=no" $USER@$HOST:$FOLDER /backup/ && \
 tar czvf - /backup > /volume/backup-$(date +"%Y-%m-%d_%H%M%S").tar.gz
-
